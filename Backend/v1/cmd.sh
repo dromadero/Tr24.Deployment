10f7c5d7bd26d1c7bbc3570d95bfe545a2c47ed7
Jakuba Nowakowski <jakub.nowakowski@vikingcruises.com>
2023-10-01 21:16:38 +0200

    we

---
+++ Backend/v1/cmd.sh
@@ -0,0 +1,12 @@
+echo ---------------------
+echo Hello from Chapter 2!
+echo ---------------------
+echo My name is:
+echo $(hostname)
+echo $(hostname -I)
+echo $(pwd)
+echo ---------------------
+echo I''m running on:
+echo $(uname -s -r -m)
+echo $(uname -a)
+echo ---------------------
